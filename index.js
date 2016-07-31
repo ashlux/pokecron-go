3b7f63c49be28c682fc348c6d7436bf515821f11
Ash Lux <arlux@monsanto.com>
2016-07-31 14:00:58 -0500

    Run every 60 seconds.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const cron = require('node-cron')
 const Tracker = require('./src/main')
 
+const CRON_ONCE_PER_MINUTE = '0 * * * * *';
+
 const homeConfig = require('/Users/arlux/.pokecron-go/home.config')
 const tracker = new Tracker(homeConfig)
-task = cron.schedule('0 * * * * *',tracker, false)
+task = cron.schedule(CRON_ONCE_PER_MINUTE, tracker, false)
 task.start()
